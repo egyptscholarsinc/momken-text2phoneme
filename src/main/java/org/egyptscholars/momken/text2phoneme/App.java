58f0927035fa04ca3f3275aae2fd97c7840b7509
amgadmadkour <amgadmadkour@users.noreply.github.com>
2015-10-15 14:28:58 -0400

    Added Tashkeel Support

--- src/main/java/org/egyptscholars/momken/text2phoneme/App.java
+++ src/main/java/org/egyptscholars/momken/text2phoneme/App.java
@@ -24,36 +24,67 @@ public class App
 		
 		ArrayList<String> phonemeEntries=new ArrayList<String>();
 		
+		System.out.println(text);
 		//String lastChar = "";
 		for(int i=0 ; i < text.length()-1; i++){
 			String currentChar = lookupTable.get(Character.toString(text.charAt(i)));
 			if (currentChar != null){
+				//Add the current character
+				phonemeEntries.add(currentChar);
 				String nextCh = lookupTable.get(Character.toString(text.charAt(i+1)));
 				//Handle Special Case of aa
 				if(nextCh != null && (currentChar.startsWith("d.") ||
 						currentChar.startsWith("t.")||
 						currentChar.startsWith("z.")||
 						currentChar.startsWith("s."))){
-					phonemeEntries.add(currentChar);
-					if(nextCh.startsWith("a")){
+					
+					if(nextCh.charAt(0) == 'a'){
 						phonemeEntries.add("a. 70 28 109 71 104");
 						i++;
-					}else if(nextCh.startsWith("i")){
+					}else if(nextCh.charAt(0) == 'i'){
 						phonemeEntries.add("i. 70 28 109 71 104");
 						i++;
-					}else if(nextCh.startsWith("u")){
+					}else if(nextCh.charAt(0) == 'u'){
 						phonemeEntries.add("u. 61 35 101 83 102");
 						i++;
 					}
-				}else{
-					phonemeEntries.add(currentChar);
 				}
 				//lastChar = currentChar;
 			}
 		}
-		phonemeEntries.add(lookupTable.get(Character.toString(text.charAt(text.length()-1))));
+		System.out.println("---"+text);
+		if(lookupTable.get(Character.toString(text.charAt(text.length()-1)))!=null)
+			phonemeEntries.add(lookupTable.get(Character.toString(text.charAt(text.length()-1))));
+		
+		//Cleanup redundant entries due to Tashkeel (i.e. i. followed by i etc.)
+		int sz = phonemeEntries.size();
+		for(int i = 0 ; i < sz-1; i++){
+			if(phonemeEntries.get(i).charAt(0) == phonemeEntries.get(i+1).charAt(0)){
+				phonemeEntries.remove(i+1);
+				sz -= 1;
+			}
+			if(phonemeEntries.get(i).startsWith("u.") && phonemeEntries.get(i+1).startsWith("a")){
+				phonemeEntries.remove(i+1);
+				sz -= 1;
+			}
+		}
+		System.out.println(phonemeEntries);
 		return phonemeEntries;
 	}
+	
+	public static String cleanBuckwalter(String buck){
+		
+		System.out.println("Before: "+buck);
+		StringBuffer buff=new StringBuffer();
+		String lst = "A'btvjHxd*rzs$SDTZEgfqklmnhwyaiu}p><O";
+		for(int i = 0 ; i < buck.length() ; i++){
+			if(lst.contains(String.valueOf(buck.charAt(i)))){
+				buff.append(String.valueOf(buck.charAt(i)));
+			}
+		}
+		return buff.toString();
+	}
+	
 	
     public static void main( String[] args ){
     	
@@ -82,15 +113,19 @@ public class App
 			
 			//Load text
 			Buckwalter b = new Buckwalter();
-			String letters;
+			String words;
 
 			while((temp = txtRdr.readLine())!= null){
 				temp = temp.replace(".", "");
-				letters = b.unicodeToBuckwalter(temp);
-		    	parts = letters.split(" ");
+				words = b.unicodeToBuckwalter(temp);
+		    	parts = words.split(" ");
 				for (int i=0 ; i<parts.length; i++){
-					phonemeEntries.addAll(createPhonemes(lookupTable, parts[i]));
-					phonemeEntries.add("_ 200");
+					String cleaned = cleanBuckwalter(parts[i]);
+					if(cleaned.length()>0){
+						ArrayList<String> ph = createPhonemes(lookupTable, cleaned);
+						phonemeEntries.addAll(ph);
+						phonemeEntries.add("_ 5");
+					}
 				}
 			}
 			
